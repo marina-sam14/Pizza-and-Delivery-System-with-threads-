b16be90d5281bc9d873b3217d8a8cef712e177d7
marina-samprovalaki <81382630+marina-samprovalaki@users.noreply.github.com>
2021-03-25 20:02:10 +0200

    Create p3180234-p3180202-p3180175-pizza2.h

---
+++ p3180234-p3180202-p3180175-pizza2.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <stdio.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int cookers = 2;
+int ovens = 5;
+int deliverers = 10;
+
+int orderlow = 1;
+int orderhigh = 5;
+int Nlow = 1;
+int Nhigh = 5;
+int prep = 1;
+int bake = 10;
+
+//xronoi delivering
+int deliverLow = 5;
+int deliverHigh=15;
